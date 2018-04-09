35b5517816364e77cd226fa004aa5d4b50d9445f
Michael Persson <michael.ake.persson@gmail.com>
2018-04-09 13:41:33 -0500

    add method encodings

--- encoding.go
+++ encoding.go
@@ -15,10 +15,20 @@ func Register(name string, encoding Encoding) {
 	encodings[name] = encoding
 }
 
+// Registered encoding is registered.
 func Registered(name string) error {
 	_, ok := encodings[name]
 	if !ok {
 		return ErrNotRegistered
 	}
 	return nil
+}
+
+// Encodings registered.
+func Encodings() []string {
+	encs := []string{}
+	for k, _ := range encodings {
+		encs = append(encs, k)
+	}
+	return encs
 }
