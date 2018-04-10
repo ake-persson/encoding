aaa645241f79fd70577e67c1e6891f388d1a8062
Michael Persson <michael.ake.persson@gmail.com>
2018-04-10 14:18:36 -0500

    add test

---
+++ toml/decoder_test.go
@@ -0,0 +1,38 @@
+package toml
+
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+
+	"github.com/mickep76/encdec"
+)
+
+func TestFromByte(t *testing.T) {
+	g := Messages{}
+	if err := encdec.FromBytes("toml", []byte(testTOML), &g); err != nil {
+		t.Error(err)
+	}
+
+	if err := deep.Equal(g, testStruct); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestFromByteWithMapString(t *testing.T) {
+	g := Messages{}
+	if err := encdec.FromBytes("toml", []byte(testTOML), &g, encdec.WithMapString()); err != encdec.ErrNotSupported {
+		t.Error(err)
+	}
+}
+
+func TestFromFile(t *testing.T) {
+	g := Messages{}
+	if err := encdec.FromFile("toml", "test.toml", &g); err != nil {
+		t.Error(err)
+	}
+
+	if err := deep.Equal(g, testStruct); err != nil {
+		t.Error(err)
+	}
+}
