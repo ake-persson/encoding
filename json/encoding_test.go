daa237d3259c299eb84097c7791dd2b89964daaa
Michael Persson <michael.ake.persson@gmail.com>
2018-04-10 08:56:31 -0500

    add test

---
+++ json/encoding_test.go
@@ -0,0 +1,29 @@
+package json
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mickep76/encdec"
+)
+
+func inList(a string, list []string) bool {
+	for _, b := range list {
+		if a == b {
+			return true
+		}
+	}
+	return false
+}
+
+func TestEncodings(t *testing.T) {
+	encs := encdec.Encodings()
+
+	if !inList("json", encs) {
+		t.Error(fmt.Errorf("encoding not in list"))
+	}
+
+	if err := encdec.Registered("json"); err != nil {
+		t.Error(err)
+	}
+}
