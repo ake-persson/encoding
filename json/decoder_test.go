2545f1f5f0339bb4bc24412c69ed343252abebf1
Michael Persson <michael.ake.persson@gmail.com>
2018-04-10 10:49:02 -0500

    add tests

--- json/decoder_test.go
+++ json/decoder_test.go
@@ -17,3 +17,14 @@ func TestFromByte(t *testing.T) {
 		t.Errorf("want:\n%+v, got:\n%+v", testMap, g)
 	}
 }
+
+func TestFromFile(t *testing.T) {
+	var g interface{}
+	if err := encdec.FromFile("json", "test.json", &g); err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(g, testMap) {
+		t.Errorf("want:\n%+v, got:\n%+v", testMap, g)
+	}
+}
--- json/encoder_test.go
+++ json/encoder_test.go
@@ -1,6 +1,8 @@
 package json
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/mickep76/encdec"
@@ -28,10 +30,30 @@ var (
 			},
 		},
 	}
+
+	tmpFile = "test.tmp"
 )
 
+func readFile(fn string) ([]byte, error) {
+	fp, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(fp)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := fp.Close(); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
 func TestToByte(t *testing.T) {
-	b, err := encdec.ToBytes("json", &testMap)
+	b, err := encdec.ToBytes("json", testMap)
 	if err != nil {
 		t.Error(err)
 	}
@@ -42,12 +64,31 @@ func TestToByte(t *testing.T) {
 }
 
 func TestToByteWithIndent(t *testing.T) {
-	b, err := encdec.ToBytes("json", &testMap, encdec.WithIndent("  "))
+	b, err := encdec.ToBytes("json", testMap, encdec.WithIndent("  "))
 	if err != nil {
 		t.Error(err)
 	}
 
 	if string(b) != testJSONIndent {
 		t.Errorf("want:\n%s, got:\n%s", testJSONIndent, string(b))
+	}
+}
+
+func TestToFile(t *testing.T) {
+	if err := encdec.ToFile("json", tmpFile, testMap); err != nil {
+		t.Error(err)
+	}
+
+	b, err := readFile(tmpFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != testJSON {
+		t.Errorf("want:\n%s, got:\n%s", testJSONIndent, string(b))
+	}
+
+	if err := os.Remove(tmpFile); err != nil {
+		t.Error(err)
 	}
 }
